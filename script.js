1729e6b1d68253fbf46376429b0bf3abb11d98ad
Cameron Nix <camerondeverenix@gmail.com>
2020-10-28 16:30:28 -0700

    working renderQuestions function

--- script.js
+++ script.js
@@ -1,78 +1,75 @@
 var start = document.getElementById("startButton");
 var timer = document.getElementById("timer");
+var intro = document.getElementById("intro");
 var quiz = document.getElementById("quiz");
 var question = document.getElementById("question");
 var choiceA = document.getElementById("a");
 var choiceB = document.getElementById("b");
 var choiceC = document.getElementById("c");
 var choiceD = document.getElementById("d");
-var answer = document.getElementById("answer");
+var answer = document.getElementById("correct");
 var score = document.getElementById("score");
-var correctAnswer;
+var choices = document.querySelector("#answerChoices");
 
 var questionIndex = 0;
+
 
 var questions = [
     {question: '1. What is the correct syntax for targetting the following using JS: div class="hello"',
     choiceA: 'document.getElementById("hello")',
     choiceB: 'document.querySelector("#hello")',
     choiceC: 'document.querySelector(".hello")',
-    choiceD: 'document.getElementById(".hello")'
-    correctAnswer: 'C',
+    choiceD: 'document.getElementById(".hello")',
+    correctAnswer: 'c'
     },
     {question: '2. What is the correct syntax for targetting the following using JS: div class="hello"',
     choiceA: 'document.getElementById("hello")',
     choiceB: 'document.querySelector("#hello")',
     choiceC: 'document.querySelector(".hello")',
-    choiceD: 'document.getElementById(".hello")'
-    correctAnswer: 'C',
+    choiceD: 'document.getElementById(".hello")',
+    correctAnswer: 'c'
     },
     {question: '3. What is the correct syntax for targetting the following using JS: div class="hello"',
     choiceA: 'document.getElementById("hello")',
     choiceB: 'document.querySelector("#hello")',
     choiceC: 'document.querySelector(".hello")',
-    choiceD: 'document.getElementById(".hello")'
-    correctAnswer: 'C',
+    choiceD: 'document.getElementById(".hello")',
+    correctAnswer: 'c'
     },
     {question: '4. What is the correct syntax for targetting the following using JS: div class="hello"',
     choiceA: 'document.getElementById("hello")',
     choiceB: 'document.querySelector("#hello")',
     choiceC: 'document.querySelector(".hello")',
-    choiceD: 'document.getElementById(".hello")'
-    correctAnswer: 'C',
+    choiceD: 'document.getElementById(".hello")',
+    correctAnswer: 'c'
     },
     {question: '5. What is the correct syntax for targetting the following using JS: div class="hello"',
     choiceA: 'document.getElementById("hello")',
     choiceB: 'document.querySelector("#hello")',
     choiceC: 'document.querySelector(".hello")',
-    choiceD: 'document.getElementById(".hello")'
-    correctAnswer: 'C',
+    choiceD: 'document.getElementById(".hello")',
+    correctAnswer: 'c'
     }
-]
+];
 
 function renderQuestion() {
-    var currentQuestion = questions[questionIndex];
-    question.textContent = currentQuestion.question;
-    choiceA.textContent = currentQuestion.choiceA;
-    choiceB.textContent = currentQuestion.choiceB;
-    choiceC.textContent = currentQuestion.choiceC;
-    choiceD.textContent = currentQuestion.choiceD;
+        question.textContent = questions[questionIndex].question;
+        choiceA.value = questions[questionIndex].choiceA;
+        choiceB.value = questions[questionIndex].choiceB;
+        choiceC.value = questions[questionIndex].choiceC;
+        choiceD.value = questions[questionIndex].choiceD;
 
-    questionIndex++;
-}
-
-function correctAnswer() {
-    var correctAnswer = document.createElement("p");
-    var lineBreak = document.createElement("hr");
-    document.body.children.
-}
-
-function incorrectAnswer() {
-
+        questionIndex++
+ 
 }
 
 start.addEventListener("click", function() {
-    var start = document.getElementById("intro");
-    start.setAttribute = ("style", "display: none");
-
+    intro.style.display = "none";
+    quiz.style.display = "block";
+    renderQuestion();
 } )
+
+choices.addEventListener("click", function() {
+    renderQuestion();
+    checkAnswer(answer);
+})
