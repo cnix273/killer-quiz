62fa58255c4f05bfaad9a8c483f0bba8a1192255
Cameron Nix <camerondeverenix@gmail.com>
2020-10-29 16:11:49 -0700

    Updated HTML and JS with code to check answers and submitting initials

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ var choiceB = document.getElementById("b");
 var choiceC = document.getElementById("c");
 var choiceD = document.getElementById("d");
 var answer = document.getElementById("correct");
+var outro = document.getElementById("outro");
 var score = document.getElementById("score");
 var choices = document.querySelector("#answerChoices");
 
 var questionIndex = 0;
+score = 0;
 
 
 var questions = [
@@ -27,7 +29,7 @@ var questions = [
     choiceB: 'document.querySelector("#hello")',
     choiceC: 'document.querySelector(".hello")',
     choiceD: 'document.getElementById(".hello")',
-    correctAnswer: 'c'
+    correctAnswer: 'd'
     },
     {question: '3. What is the correct syntax for targetting the following using JS: div class="hello"',
     choiceA: 'document.getElementById("hello")',
@@ -41,7 +43,7 @@ var questions = [
     choiceB: 'document.querySelector("#hello")',
     choiceC: 'document.querySelector(".hello")',
     choiceD: 'document.getElementById(".hello")',
-    correctAnswer: 'c'
+    correctAnswer: 'd'
     },
     {question: '5. What is the correct syntax for targetting the following using JS: div class="hello"',
     choiceA: 'document.getElementById("hello")',
@@ -53,23 +55,80 @@ var questions = [
 ];
 
 function renderQuestion() {
+    for (i=0; i<= questions.length; i++) {
         question.textContent = questions[questionIndex].question;
         choiceA.value = questions[questionIndex].choiceA;
         choiceB.value = questions[questionIndex].choiceB;
         choiceC.value = questions[questionIndex].choiceC;
         choiceD.value = questions[questionIndex].choiceD;
+    }
+};
 
-        questionIndex++
- 
+
+
+var seconds = 60;
+
+function time(minutes, seconds) {
+    var seconds = 60;
+    var mins = minutes
+
+    function countdown() {
+        var timeout;
+        var counter = document.getElementById("timer");
+
+        var current_minutes = mins - 1
+        seconds--;
+
+        counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+        
+        if (seconds > 0) {
+            timeout = setTimeout(countdown, 1000);
+        }
+
+        else {
+            mins--;
+            seconds = 60;
+            timeout = setTimeout(countdown, 1000);
+        }
+
+        if (current_minutes < 0) {
+            return;
+        }
+
+    }
+    countdown();
 }
+
+function gameOver() {
+    quiz.style.display = "none";
+    outro.style.display = "block";
+};
 
 start.addEventListener("click", function() {
     intro.style.display = "none";
     quiz.style.display = "block";
-    renderQuestion();
-} )
 
-choices.addEventListener("click", function() {
+
     renderQuestion();
-    checkAnswer(answer);
-})
+    time(2);
+});
+
+choices.addEventListener("click", function(event) {
+    var checkedAnswer =  event.target.id
+    if (checkedAnswer == questions[questionIndex].correctAnswer) {
+        score++;
+        answer.textContent = "Correct :)";
+    }
+    else if (checkedAnswer !== questions[questionIndex].correctAnswer) {
+        answer.textContent = "Incorrect :(";
+        seconds = seconds - 10;
+    }
+    questionIndex++
+    if (questionIndex < questions.length) {
+        renderQuestion();
+    }
+    else if (questionIndex = questions.length) {
+        gameOver();
+    }
+    console.log(score);
+});
