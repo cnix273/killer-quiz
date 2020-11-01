4d994205b269f16956fb822a2630c53bc1b422b5
Cameron Nix <camerondeverenix@gmail.com>
2020-10-31 23:58:31 -0700

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# killer-quiz
+# Killer Quiz
+
+## Summary
+
+This site houses a 5-question multiple choice quiz about JavaScript, HTML, and CSS basics. The timer for the quiz is displayed in the navbar and the initial time limit is 75 seconds, the timer being reduced by 10 seconds every time a question is answered incorrectly. The site is dynamic, the web application displaying the next question as each is answered as well as whether of not the user got that question right. Once the timer runs out or every question has been answered, the quiz is over and the user is prompted to enter in their initials and submit their score. The scores are stored in a highscore table which saves the top 5 scores and can be accessed at any time using a link in the navbar. They also have the option to clear their score table or go back to the start and attempt the quiz again.
+
+This site is responsive to ensure that the site renders well on a variety of devices and window or screen sizes.
+
+## Installation
+
+Clone the repository from github.
+
+* git clone https://github.com/cnix273/killer-quiz.git
+
+## Usage
+
+**Technologies Used**
+* HTML - Used to create the structure of the site
+* CSS - Used to style the HTML Elements
+* JavaScript - Used to make the site dynamic
+* GitHub - Hosts the repository that can be deployed to GitHub Pages
+
+The repository includes the index.html, script.js, and style.css. Repository structure must be maintained for proper usage of the index.html file.
+
+Deployed application: https://cnix273.github.io/killer-quiz/.
+
+## Credits
+
+**Author:** Cameron Nix
+* [LinkedIn](https://www.linkedin.com/in/cameron-nix-a74aa1109/)
+* [GitHub](https://github.com/cnix273)
+
+## Contributing
+
+Pull requests are welcome.
+
+To contribute to this refactor, clone this repo locally and commit your code on a separate branch.
+
+## License
+
+Copyright (c) 2020 Cameron Nix
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var start = document.getElementById("startButton");
+// Variables retrieved from HTML using id
+var start = document.getElementById("start-button");
 var counter = document.getElementById("timer");
 var intro = document.getElementById("intro");
 var quiz = document.getElementById("quiz");
@@ -10,35 +11,45 @@ var choiceD = document.getElementById("d");
 var answer = document.getElementById("answer");
 var result = document.getElementById("result");
 var outro = document.getElementById("outro");
-var score = document.getElementById("score");
-var choices = document.querySelector("#answerChoices");
-var finalScore = document.querySelector("#finalScore");
-var submitScore = document.getElementById("submitScore");
+var choices = document.querySelector("#answer-choices");
+var finalScore = document.querySelector("#final-score");
+var submitScore = document.getElementById("submit-score");
+var highscorePage = document.getElementById("highscores");
+var highscoreList = document.getElementById("highscore-list")
+var link = document.getElementById("link");
+var backBtn = document.getElementById("back");
+var clearBtn = document.getElementById("clear");
 
+// JS Variables
 var questionIndex = 0;
+var checkedAnswer;
+var deductTime = false;
 score = 0;
+var seconds = 75;
+var timeout;
 
 
+// Array of quiz questions
 var questions = [
-    {question: '1. What is the correct syntax for targetting the following using JS: div class="hello"',
-    choiceA: 'document.getElementById("hello")',
-    choiceB: 'document.querySelector("#hello")',
-    choiceC: 'document.querySelector(".hello")',
-    choiceD: 'document.getElementById(".hello")',
-    correctAnswer: 'c'
+    {question: '1. What does HTML stand for?',
+    choiceA: 'high tech material letters',
+    choiceB: 'hypertext markup language',
+    choiceC: 'happy text markup language',
+    choiceD: 'hello to my love',
+    correctAnswer: 'b'
     },
-    {question: '2. What is the correct syntax for targetting the following using JS: div class="hello"',
-    choiceA: 'document.getElementById("hello")',
-    choiceB: 'document.querySelector("#hello")',
-    choiceC: 'document.querySelector(".hello")',
-    choiceD: 'document.getElementById(".hello")',
+    {question: '2. What is the name of the data type that has a value of true or false',
+    choiceA: 'string',
+    choiceB: 'array',
+    choiceC: 'object',
+    choiceD: 'boolean',
     correctAnswer: 'd'
     },
-    {question: '3. What is the correct syntax for targetting the following using JS: div class="hello"',
-    choiceA: 'document.getElementById("hello")',
-    choiceB: 'document.querySelector("#hello")',
-    choiceC: 'document.querySelector(".hello")',
-    choiceD: 'document.getElementById(".hello")',
+    {question: '3. What is the name of the following method for accessing values in an object: "var x = object.item;',
+    choiceA: 'period notation',
+    choiceB: 'object notation',
+    choiceC: 'bracket notation',
+    choiceD: 'dot notation',
     correctAnswer: 'c'
     },
     {question: '4. What is the correct syntax for targetting the following using JS: div class="hello"',
@@ -57,6 +68,7 @@ var questions = [
     }
 ];
 
+// Render quiz questions
 function renderQuestion() {
     for (i=0; i<= questions.length; i++) {
         question.textContent = questions[questionIndex].question;
@@ -65,47 +77,13 @@ function renderQuestion() {
         choiceC.value = questions[questionIndex].choiceC;
         choiceD.value = questions[questionIndex].choiceD;
     }
-};/*
+};
 
-var seconds = 60;
 
-function time(minutes, seconds) {
-    var seconds = 60;
-    var mins = minutes
-
-    function countdown() {
-        var timeout;
-        var counter = document.getElementById("timer");
-
-        var current_minutes = mins - 1
-        seconds--;
-
-        counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-        
-        if (seconds > 0) {
-            timeout = setTimeout(countdown, 1000);
-        }
-
-        else {
-            mins--;
-            seconds = 60;
-            timeout = setTimeout(countdown, 1000);
-        }
-
-        if (current_minutes < 0) {
-            return;
-        }
-
-    }
-    countdown();
-}*/
-
-var seconds = 76;
-
+// Set timer
 function time(seconds) {
 
     function countdown() {
-        var timeout;
 
         seconds--;
 
@@ -115,51 +93,257 @@ function time(seconds) {
             timeout = setTimeout(countdown, 1000);
         }
 
-        else {
-            gameOver();
-            return;
+        if (deductTime) {
+            clearTimeout(timeout);
+            seconds -= 10;
+            counter.innerHTML = "Timer: " + String(seconds);
+            timeout = setTimeout(countdown, 1000);
+            deductTime = false;
         }
+
+        if (seconds > 0 && questionIndex == questions.length || seconds < 1) {
+            clearTimeout(timeout);
+            gameOver();
+        }
+
     }
     countdown();
 }
 
+// Quiz over
 function gameOver() {
     quiz.style.display = "none";
-    result.style.display = "none";
+    result.style.display = "block";
     outro.style.display = "block";
 
-    finalScore.innerHTML = "Your final score is " + score + "/5!";
- };
+    finalScore.innerHTML = "Your final score is " + score + "/" + questions.length + "!";
+}
 
-start.addEventListener("click", function() {
+// Declare highscore variables
+
+ var highscore1 = document.getElementById("highscore1");
+ var highscore2 = document.getElementById("highscore2");
+ var highscore3 = document.getElementById("highscore3");
+ var highscore4 = document.getElementById("highscore4");
+ var highscore5 = document.getElementById("highscore5");
+
+ // Add score and initials to highscore list
+function highscore() {
+    intro.style.display = "none";
+    quiz.style.display = "none";
+    outro.style.display = "none";
+    result.style.display = "none";
+    highscorePage.style.display = "block";
+    counter.style.display = "none";
+    link.style.display = "none";
+
+    var initials1 = localStorage.getItem(initials1);
+    var initials2 = localStorage.getItem(initials2);
+    var initials3 = localStorage.getItem(initials3);
+    var initials4 = localStorage.getItem(initials4);
+    var initials5 = localStorage.getItem(initials5);
+   
+    var score1 = localStorage.getItem(score1);
+    var score2 = localStorage.getItem(score2);
+    var score3 = localStorage.getItem(score3);
+    var score4 = localStorage.getItem(score4);
+    var score5 = localStorage.getItem(score5);
+
+    var initials = document.getElementById("initials").value;
+
+
+    if (score1 == null) {
+        score1 = score;
+        initials1 = initials.toUpperCase();
+        localStorage.setItem("score1", score1)
+        localStorage.setItem("initials1", initials1)
+        highscore1.innerText = "1. " + initials1 + " - " + score1;
+        return;
+    }
+    if (score2 == null) {
+        score2 = score;
+        initials2 = initials.toUpperCase();
+        localStorage.setItem("score2", score2)
+        localStorage.setItem("initials2", initials2)
+        highscore2.innerText = "2. " + initials2 + " - " + score2;
+        return;
+    }
+    if (score3 == null) {
+        score3 = score;
+        initials3 = initials.toUpperCase();
+        localStorage.setItem("score3", score3)
+        localStorage.setItem("initials3", initials3)
+        highscore3.innerText = "3. " + initials3 + " - " + score3;
+        return;
+    }
+    if (score4 == null) {
+        score4 = score;
+        initials4 = initials.toUpperCase();
+        localStorage.setItem("score4", score4)
+        localStorage.setItem("initials4", initials4)
+        highscore4.innerText = "4. " + initials4 + " - " + score4;
+        return;
+    }
+    if (score5 == null) {
+        score5 = score;
+        initials5 = initials.toUpperCase();
+        localStorage.setItem("score5", score5)
+        localStorage.setItem("initials5", initials5)
+        highscore5.innerText = "5. " + initials5 + " - " + score5;
+        return;
+    }
+
+    if (score > score1) {
+        score1 = score;
+        initials1 = initials.toUpperCase();
+        localStorage.setItem("score1", score1)
+        localStorage.setItem("initials1", initials1)
+        highscore1.innerText = "1. " + initials1 + " - " + score1;
+        return;
+    }
+    if (score > score2) {
+        score2 = score;
+        initials2 = initials.toUpperCase();
+        localStorage.setItem("score2", score2)
+        localStorage.setItem("initials2", initials2)
+        highscore2.innerText = "2. " + initials2 + " - " + score2;
+        return;
+    }
+    if (score > score3) {
+        score3 = score;
+        initials3 = initials.toUpperCase();
+        localStorage.setItem("score3", score3)
+        localStorage.setItem("initials3", initials3)
+        highscore3.innerText = "3. " + initials3 + " - " + score3;
+        return;
+    }
+    if (score > score4) {
+        score4 = score;
+        initials4 = initials.toUpperCase();
+        localStorage.setItem("score4", score4)
+        localStorage.setItem("initials4", initials4)
+        highscore4.innerText = "4. " + initials4 + " - " + score4;
+        return;
+    }
+    if (score > score5) {
+        score5 = score;
+        initials5 = initials.toUpperCase();
+        localStorage.setItem("score5", score5)
+        localStorage.setItem("initials5", initials5)
+        highscore5.innerText = "5. " + initials5 + " - " + score5;
+        return;
+    }
+
+    if (score1 !== null && score < score1) {
+        highscore1.innerText = "1. " + initials1 + " - " + score1;
+        return;
+    }
+    if (score2 !== null && score < score2) {
+        highscore2.innerText = "2. " + initials2 + " - " + score2;
+        return;
+    }
+    if (score3 !== null && score < score3) {
+        highscore3.innerText = "1=3. " + initials3 + " - " + score3;
+        return;
+    }
+    if (score4 !== null && score < score4) {
+        highscore4.innerText = "4. " + initials4 + " - " + score4;
+        return;
+    }
+    if (score5 !== null && score < score5) {
+        highscore5.innerText = "5. " + initials5 + " - " + score5;
+        return;
+    }
+}
+
+// Start quiz button
+start.addEventListener("click", function start() {
     intro.style.display = "none";
     quiz.style.display = "block";
-
+    result.style.display = "block";
+    questionIndex = 0;
+    score = 0;
 
     renderQuestion();
     time(seconds);
 });
 
+// Select answer button and check to see if it's correct
 choices.addEventListener("click", function(event) {
-    var checkedAnswer =  event.target.id
+    checkedAnswer = event.target.id
+
     if (checkedAnswer == questions[questionIndex].correctAnswer) {
         score++;
         answer.textContent = "Correct :)";
     }
     else if (checkedAnswer !== questions[questionIndex].correctAnswer) {
         answer.textContent = "Incorrect :(";
-        seconds = seconds - 10;
+        deductTime = true;
     }
+
     questionIndex++
+
     if (questionIndex < questions.length) {
         renderQuestion();
     }
+
     else if (questionIndex = questions.length) {
         gameOver();
     }
-    console.log(score);
 });
 
-submitScore.addEventListener("click", function() {
+// Submit initials button
+submitScore.addEventListener("click", function(event) {
+    event.preventDefault();
+    var initials = document.getElementById("initials").value;
+    console.log(initials);
+    if (initials == '') {
+        alert("Please enter your initials so that we can save your score!!");
+    }
+    else {
+        highscore();
+    }
+});
 
+// View highscores button function
+link.addEventListener("click", function() {
+    intro.style.display = "none";
+    quiz.style.display = "none";
+    outro.style.display = "none";
+    result.style.display = "none";
+    highscorePage.style.display = "block";
+    counter.style.display = "none";
+    link.style.display = "none";
+});
+
+// Restart the quiz button function
+backBtn.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    highscorePage.style.display = "none";
+    intro.style.display = "block";
+    counter.style.display = "block";
+    link.style.display = "block";
+});
+
+// Clear highscores button function
+clearBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    localStorage.clear();
+    highscore1.innerText = "";
+    highscore2.innerText = "";
+    highscore3.innerText = "";
+    highscore4.innerText = "";
+    highscore5.innerText = "";
+
+    initials1 = localStorage.getItem(initials1);
+    initials2 = localStorage.getItem(initials2);
+    initials3 = localStorage.getItem(initials3);
+    initials4 = localStorage.getItem(initials4);
+    initials5 = localStorage.getItem(initials5);
+   
+    score1 = localStorage.getItem(score1);
+    score2 = localStorage.getItem(score2);
+    score3 = localStorage.getItem(score3);
+    score4 = localStorage.getItem(score4);
+    score5 = localStorage.getItem(score5);
 });
